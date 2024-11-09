47e75e6b0805b31dd392b3e9bd923b24674738d0
Harith-07 <harith.v@ikomet.com>
2024-11-09 18:09:28 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,5 @@ app.use('/api', cardRoutes);
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);    
+  console.log(`Server is running on port ${PORT}`);     
 });
