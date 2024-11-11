35fcd1797fbec5c658c9c74c06cbcf4ede673d15
kaleeswaran18 <70526366+kaleeswaran18@users.noreply.github.com>
2024-11-11 23:49:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-// Middleware to allow cross-origin requests from your frontend (localhost:3000)
+// Middleware to allow cross-origin requests from your frontend (localhost:3000) 
 app.use(cors());
 
 // Middleware
